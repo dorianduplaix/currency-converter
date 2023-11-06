387effa4182b36b362129db65ce6e88f11293730
dorianduplaix <dorian.duplaix@ekonoo.com>
2023-11-06 14:55:04 +0100

    Initial Commit

---
+++ currency-converter/currency_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  currency_converterApp.swift
+//  currency-converter
+//
+//  Created by Dorian Duplaix on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct currency_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
